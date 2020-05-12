9b32d8c6f4180c9b77b5c7409df49efb352b6cde
Michael Kim <m.kim4247@gmail.com>
2020-05-12 16:42:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 // Write your code in this file!
-function scuberGreetingForFeet(num) {
-  if (num <= 400) {
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
     return 'This one is on me!';
   }
-  else if (num > 2000 && num < 2500) {
+  else if (feet > 2000 && feet < 2500) {
     return 'I will gladly take your thirty bucks.';
   }
   else {
     return 'No can do.';
   }
 }
-
 function ternaryCheckCity(city) {
-  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  return (city === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
-
 function switchOnCharmFromTip(tip) {
-  if (tip === 'generous') {
-    return 'Thank you so much.';
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
   }
-  else if (tip === 'not as generous') {
-    return 'Thank you.';
-  }
-  else {
-    return 'Bye.';
-  }
-
 }
